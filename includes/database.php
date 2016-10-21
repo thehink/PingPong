75e720f8b1a1365bb06b982f277310397f43ee6f
benja <benja280@gmail.com>
2016-10-21 14:54:21 +0200

    Added database file. Should communicate with database from this file in the future

--- includes/database.php
+++ includes/database.php
@@ -9,15 +9,48 @@ Database::getNames() -> [Array of names]
 */
 
 class Database{
-  private $players = [
-    'Robert N', 'Lars K', 'Joakim R', 'Benjamin R', 'Marie E', 'Maria GN',
-                   'Axel B', 'Amin E-R', 'André H', 'Carl Å', 'Christian B', 'Katarina C',
-                   'Erica G', 'Jeremy D', 'Kristjan F', 'Mathias K', 'Signe B', 'Staffan M',
-                   'Victor O', 'Max S', 'Johannes T', 'Vincent K', 'Harry E (Revolutionist)'
+  private static $players = [
+    'Robert N',
+    'Lars K',
+    'Joakim R',
+    'Benjamin R',
+    'Marie E',
+    'Maria GN',
+    'Axel B',
+    'Amin E-R',
+    'André H',
+    'Carl Å',
+    'Christian B',
+    'Katarina C',
+    'Erica G',
+    'Jeremy D',
+    'Kristjan F',
+    'Mathias K',
+    'Signe B',
+    'Staffan M',
+    'Victor O',
+    'Max S',
+    'Johannes T',
+    'Vincent K',
+    'Harry E (Revolutionist)'
   ];
 
+  static function readNamesFromTxt(){
+
+  }
 
   static function getNames(){
-    return $players;
+    asort(self::$players);
+    return self::$players;
+  }
+
+  static function getNamesByIds($arr){
+    $names = self::getNames();
+    $selectedNames = [];
+
+    foreach ($arr as $i => $id) {
+      array_push($selectedNames, $names[$id]);
+    }
+    return $selectedNames;
   }
 }
