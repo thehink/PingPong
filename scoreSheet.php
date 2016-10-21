bea2ae0710e46d6755e6b6893816d5973a186a86
Lars Kajes <lars@kajes.se>
2016-10-21 10:35:46 +0200

    Added simple styling and some cleanup in scoreSheet.php

--- scoreSheet.php
+++ scoreSheet.php
@@ -2,35 +2,37 @@
 <html>
   <head>
     <meta charset="utf-8">
+	<link rel="stylesheet" href="css/style.css">
     <title>Ping pong scores</title>
   </head>
   <body>
-    <header>
-      <?php if (!isset($_POST['start'])): ?>
-      <h2>Welcome to Ping Pong Score Tracker 3000</h2>
-      <h3>Press START to begin a new round!</h3>
-      <form method="POST">
-        <button type="submit" name="start">START</button>
-      </form>
-    <?php endif; ?>
-    </header>
-    <?php
-    include 'scoreSheetFunctions.php';
-    include 'gameOn.php';
-    $scores = getScores();
+		<main class="mainWrapper">
+			<header>
+				<?php if (!isset($_POST['start'])): ?>
+					<h2>Ping Pong Score Tracker <br><span>3000</span></h2>
+					<form method="POST">
+						<button type="submit" name="start">NEW ROUND</button>
+					</form>
+				<?php endif; ?>
+			</header>
+			<?php
+			include 'scoreSheetFunctions.php';
+			include 'gameOn.php';
+			$scores = getScores();
 
 
-    if (isset($_POST['start'])) {
-      echo 'How many players? <form method="POST" action="names.php"><input type="number" name="numPlayers"></input><button>submit</button></form>';
-    }
+			if (isset($_POST['start'])) {
+				echo 'How many players? <form method="POST" action="names.php"><input type="number" name="numPlayers"></input><button>submit</button></form>';
+			}
 
 
-    if (!empty($scores)) {
-      foreach ($scores as $key => $value) {
-        echo '<button>'.$key.'</button>';
-      }
-    }
+			if (!empty($scores)) {
+				foreach ($scores as $key => $value) {
+					echo '<button>'.$key.'</button>';
+				}
+			}
 
-    ?>
+			?>
+		</main>
   </body>
 </html>
