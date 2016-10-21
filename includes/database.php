bc787d70e70008755641252f3e37bba39f0596d4
benja <benja280@gmail.com>
2016-10-21 14:54:21 +0200

    Added includes/database.php

---
+++ includes/database.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Class Database
+
+usage:
+include_once('database.php');
+
+Database::getNames() -> [Array of names]
+*/
+
+class Database{
+  private $players = [
+    'Robert N', 'Lars K', 'Joakim R', 'Benjamin R', 'Marie E', 'Maria GN',
+                   'Axel B', 'Amin E-R', 'André H', 'Carl Å', 'Christian B', 'Katarina C',
+                   'Erica G', 'Jeremy D', 'Kristjan F', 'Mathias K', 'Signe B', 'Staffan M',
+                   'Victor O', 'Max S', 'Johannes T', 'Vincent K', 'Harry E (Revolutionist)'
+  ];
+
+
+  static function getNames(){
+    return $players;
+  }
+}
