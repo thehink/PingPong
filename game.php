2ba77fa281f01453994aad8ae030334c81c6bf14
benja <benja280@gmail.com>
2016-10-21 22:20:14 +0200

    Fixed a bug causing the footer to not display

--- game.php
+++ game.php
@@ -9,36 +9,46 @@ include_once('includes/database.php');
 include_once('includes/score.php');
 
 $playerIds = $_POST['players'];
-
-//player clicked a name so we should add the score and remove player from array
 $playerCount = (int)$_POST['playerCount'];
+
+//get the players by the ids supplied in the post request.
+//This should be keepen track of in the database in the future
 $players = Database::getPlayersByIds($playerIds);
 
+
+//player clicked a name so we should add the score and remove player from array
 if(isset($_POST['addScore'])){
+  //get the index of the player who lost
   $playerIndex = (int)$_POST['addScore'];
   $player = $players[$playerIndex];
 
+//remove the player who lost from the current tables of players
   array_splice($playerIds, $playerIndex, 1);
   array_splice($players, $playerIndex, 1);
 
-//normal score
+//normal score (TotalPlayerCount - PlayersLeft) * 10
   $score = ($playerCount - count($players)) * 10;
 
+//only 1 player left in array of players. So that player has to be the winner.
   if(count($players) === 1){
     $winner = $players[0];
-    $winnerScore = $score + 30;
+    $winnerScore = $score + 30; //bonus score for winner is defined here
     echo "Wohooo " . $winner['name'] . " has won<br>";
-    echo $player['name'] . ' got ' . $score . ' points<br>';
     echo $winner['name'] . '(winner) got ' . $winnerScore . ' points<br>';
-    echo '<a href="scoreboard.php">Show Scoreboard</a>';
+
+    //add score of winner
     Score::addScore($winner, $winnerScore, 1);
+
+    //add score for second place
     Score::addScore($player, $score, 0);
-    exit;
+    $players = [];
   }
 
+  //add score for any other place
   Score::addScore($player, $score, 0);
-  echo $player['name'] . ' got ' . $score . ' points<br>';
+  echo $player['name'] . ' got ' . $score . ' points for ' . (count($playerIds)+1) . ' place<br>';
 }
+
 
 foreach ($players as $i => $player) {?>
   <form class="" action="index.php" method="post">
