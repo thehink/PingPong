127cde5950d5dd88864117a4035ca77d96e9a8c3
benja <benja280@gmail.com>
2016-10-22 12:14:39 +0200

    PingPongGame class

--- lib/game.php
+++ lib/game.php
@@ -4,16 +4,40 @@ require_once('database.php');
 class PingPongGame{
 
   private $gameId;
+  private $gameInfo;
+  private $players;
 
-  function __construct($gameId = NULL) {
+  function __construct($gameId = NULL, $players = []) {
     $this->gameId = $gameId;
+
+    if($this->gameId){
+      $this->$gameInfo = Database::getGame($gameId);
+      if(!$game){
+        throw new Exception('You tried to play a game that doesnt exist in database!');
+      }
+    }else{
+      Database::addGame($gameId, );
+      $this->$gameId = Database::addGame($gameId, count($players));
+      foreach ($players as $value) {
+        
+      }
+    }
+
+    $this->players = Database::getPlayersByGame($gameId);
+
   }
 
   function addPlayers($players){
 
   }
 
-  function round(){
+  function newRound($loser){
 
   }
+
+  public function newGame($name, $players){
+    //$this->gameId = Database::newGame($name, count($players));
+    //$this->gameId = Database::newGame($name, count($players));
+  }
+
 }
