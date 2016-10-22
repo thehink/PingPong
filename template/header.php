b25c6030e4251f65199dd744cc4256db3e53ceaa
benja <benja280@gmail.com>
2016-10-22 12:09:52 +0200

    Added templates

---
+++ template/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<link rel="stylesheet" href="css/style.css">
+    <title>Ping pong scores</title>
+  </head>
+  <body>
+		<main class="mainWrapper">
+			<header>
+				<h2>Ping Pong Score Master <br><span>3000</span></h2>
+			</header>
