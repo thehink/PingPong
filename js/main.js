c89b5a5c5bfeef5f0ea28d776d143ac06a6c8b82
benja <benja280@gmail.com>
2016-10-23 16:24:55 +0200

    Added a javascript to dynamically build the choose players form

--- js/main.js
+++ js/main.js
@@ -1,64 +1,183 @@
 'use strict';
 
-// Initiate Player Array
-const classMates = ['', 'Robert N', 'Lars K', 'Joakim R', 'Benjamin R', 'Marie E', 'Maria GN',
-		               'Axel B', 'Amin E-R', 'André H', 'Carl Å', 'Christian B', 'Katarina C',
-		               'Erica G', 'Jeremy D', 'Kristjan F', 'Mathias K', 'Signe B', 'Staffan M',
-		               'Victor O', 'Max S', 'Johannes T', 'Vincent K', 'Harry E (Revolutionist)'];
-classMates.sort(); // ...and sort it
+const SortedPlayers = players.sort(function(a, b){
+	var nameA = a.firstname.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.firstname.toUpperCase(); // ignore upper and lowercase
+	if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  // names must be equal
+  return 0;
+});
 
-// Get some elements and assign variables
-const playCount = document.querySelector("#playerCount");
-const selectContainer = document.querySelector("#selectContainer");
+let selectedPlayers = {};
+let playerCount = 2;
 
-// Remove children if they exist
-while (selectContainer.hasChildNodes()) {
-  selectContainer.removeChild(selectContainer.lastChild);
-};
 
-// Function for dynamically adding selectboxes
-const selectBoxes = function() {
+// Get PlayerCount form
+const playCount = document.querySelector("#playerCountForm");
 
-    // Dynamically add select options
-    if (!playCount.value) {
-        selectContainer.style.display = 'none'; // Hide Select-container if empty.
-    } else {
-        selectContainer.style.display = 'block';
-        for (let i = playCount.value; i > 0; i--) {
+function onPlayerCountSubmit(event){
+	//prevent submit of playercount
+	event.preventDefault();
 
-          // First we add the label
-          const label = selectContainer.appendChild(document.createElement('label'));
-          label.htmlFor = 'players[]';
-          label.className = 'label';
-          label.innerHTML = 'Player:';
+	//get value of player count input
+	playerCount = playCount.querySelector('input').value;
 
-          // Add the right amount of selectboxes
-          selectContainer.appendChild(document.createElement('select'));
-          const select = document.querySelectorAll('select');
-          select.forEach( function (singleSelect) {
-            singleSelect.className = 'selectBox';
-            singleSelect.name = 'player[]';
-          });
-        }
-    }
-};
+	//reset selected players
+	selectedPlayers = [];
 
-// Function for adding all the names to the options
-const optionBoxes = function() {
+//define error variable
+	let error;
 
-  // Place selectBoxes in variable
-  const selectBoxes = document.querySelectorAll('.selectBox');
+//check for errors
+	if(playerCount > SortedPlayers.length){
+		error = 'Max ' + SortedPlayers.length + ' players!';
+	}
 
-  // Place all the options in each selectbox
-  selectBoxes.forEach(function(box){
-    for (let i = 0; i < classMates.length; i++) {
-      const optionBox = box.appendChild(document.createElement('option'));
-      optionBox.value = i;
-      optionBox.innerHTML = classMates[i];
-    }
-  });
-};
+	if(playerCount < 2){
+		error = 'Min 2 players!';
+	}
 
-// Start eventhandler for selectBoxes function and actually use it
-playCount.addEventListener('change', selectBoxes());
-optionBoxes();
+//delete previous nodes
+	const oldNodes = document.querySelectorAll("#playersForm, .nameFieldErrorMessage");
+	if(oldNodes){
+		oldNodes.forEach(function(node){
+			node.remove();
+		});
+	}
+
+//if errors build a span and display errors or build player select form
+	let node;
+	if(error){
+		node = document.createElement('span');
+		node.className = 'nameFieldErrorMessage';
+		node.innerText = error;
+	}else{
+		node = buildPlayerOptions(playerCount);
+	}
+
+	//insert after playerCount form
+	playCount.parentNode.insertBefore(node, playCount.nextSibling);
+}
+
+function buildPlayerOptions(playerCount){
+	//start building form
+	const form = document.createElement('form');
+	form.id = 'playersForm';
+	form.method = 'POST';
+	form.action = 'index.php';
+
+	const label = document.createElement('label');
+	label.className = 'label';
+	label.innerText = 'Enter player names';
+
+	//form.appendChild(label);
+
+	for(let i = 0; i < playerCount; ++i){
+		let selectLabel = document.createElement('label');
+		selectLabel.innerText = 'Player ' + (i+1);
+
+		let select = document.createElement('select');
+		select.className = 'selectBox';
+    select.name = 'selectedPlayers[]';
+		select.id = 'player'+(i+1);
+
+		let option = document.createElement('option');
+		option.value = -1;
+		option.innerText = 'Select player...';
+		select.appendChild(option);
+
+		for(let j = 0; j < players.length; ++j){
+			let option = document.createElement('option');
+			option.value = players[j].id;
+	    option.innerText = players[j].firstname;
+			select.appendChild(option);
+		}
+
+		select.addEventListener('change', onOptionChange);
+
+		form.appendChild(selectLabel);
+		form.appendChild(select);
+
+	}
+
+	let button = document.createElement('button');
+	button.type = 'submit';
+	button.innerText = 'New Round';
+	form.appendChild(button);
+
+	form.addEventListener('submit', function(event){
+		//check to make sure no select is empty
+		for(let i = 0; i < playerCount; ++i){
+			if(!selectedPlayers['player' + (i+1)]){
+				alert('You need to select all players!');
+				return event.preventDefault();
+			}
+		}
+	});
+
+	return form;
+}
+
+//this is where the magic happens
+function onOptionChange(event){
+	const playerId = parseInt(event.target.value);
+
+//store selected player so we can access it later
+	selectedPlayers[event.target.id] = SortedPlayers.find(function(player){
+		return player.id === playerId;
+	});
+
+//stor all selected player ids here
+	let usedPlayers = [];
+
+//push all selected players to the usedPlayers array
+	for(let i = 0; i < playerCount; ++i){
+		const select = document.querySelector("#player" + (i+1));
+		usedPlayers.push(parseInt(select.value));
+	}
+
+//loop through all select boxes
+	for(let i = 0; i < playerCount; ++i){
+		const select = document.querySelector("#player" + (i+1));
+		const selectedPlayer = selectedPlayers[select.id];
+
+//filter out all used players
+		let filteredPlayers = SortedPlayers.filter(function(player){
+			return usedPlayers.indexOf(parseInt(player.id)) === -1;
+		});
+
+//remove all options
+		while (select.firstChild) {
+		  select.removeChild(select.firstChild);
+		}
+
+//if we have selected a player for this dropdown previously append it first.
+//else append a default Select player... option
+		if(selectedPlayer){
+			let option = document.createElement('option');
+			option.value = selectedPlayer.id;
+			option.innerText = selectedPlayer.firstname;
+			select.appendChild(option);
+		}else{
+			let option = document.createElement('option');
+			option.value = -1;
+			option.innerText = 'Select player...';
+			select.appendChild(option);
+		}
+
+//then add the rest of the filtered players to the dropdown
+		filteredPlayers.forEach(function(player){
+			let option = document.createElement('option');
+			option.value = player.id;
+	    option.innerText = player.firstname;
+			select.appendChild(option);
+		})
+	}
+}
+
+playCount.addEventListener('submit', onPlayerCountSubmit);
--- template/playerCount.php
+++ template/playerCount.php
@@ -1,5 +1,10 @@
+<script>
+'use strict';
+//print out the players from php
+const players = <?=json_encode($players)?>;
+</script>
 <section class="mainContent">
-					<form method="POST" action="index.php">
+					<form method="POST" action="index.php" id="playerCountForm">
 						<label for="playCount" class="label">How many players?</label>
 						<input type="number" name="playerCount" value="<?=$playerCount?>">
             <?=isset($errors) && isset($errors['playerCount']) ? '<span class="nameFieldErrorMessage">'.$errors['playerCount'].'</span>' : ''?>
@@ -7,3 +12,5 @@
 						<button type="submit" name="start">Set Players</button>
 					</form>
 </section>
+<!--Include javascript for select players -->
+<script src="js/main.js" charset="utf-8"></script>
