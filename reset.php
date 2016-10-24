e37251cc8d131f461a6d5b7fba28a5ffa457cd77
thehink <benja280@gmail.com>
2016-10-24 12:06:02 +0200

    Changed button to submit

--- reset.php
+++ reset.php
@@ -1,12 +1,10 @@
 <?php
 require_once('lib/header.php');
 
-echo 'Delete ALL the scores?!?!?! <form method="POST"><input type="button" name="Yes" value="Yes"></input></form>';
+echo 'Delete ALL the scores?!?!?! <form method="POST"><input type="submit" name="Yes" value="Yes"></input></form>';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['Yes'])) {
   Database::query('DELETE FROM games');
   Database::query('DELETE FROM results');
   echo "Done!";
 }
-
-
