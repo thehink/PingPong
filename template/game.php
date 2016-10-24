a5c507a30251486291a12b57174980bccd7a57d9
thehink <benja280@gmail.com>
2016-10-24 11:04:57 +0200

    Fixed character encoding

--- template/game.php
+++ template/game.php
@@ -4,7 +4,7 @@
   <form class="" action="game.php" method="post">
     <input type="hidden" name="new_round" value="1">
     <input type="hidden" name="eliminated_player" value="<?=$player['id']?>">
-    <button type="submit"><?=$player['firstname'] . ' ' . $player['lastname'][0]?></button>
+    <button type="submit"><?=$player['firstname'] . ' ' . mb_substr($player['lastname'],0,1)?></button>
   </form>
 
 <?php endforeach; ?>
