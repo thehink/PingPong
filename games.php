87e34a3ab382957e63a3a490380820ce38cd0b76
thehink <benja280@gmail.com>
2016-10-27 16:25:51 +0200

    Fixed game in progress link remaining even if that game was removed

--- games.php
+++ games.php
@@ -2,6 +2,9 @@
 require_once('lib/header.php');
 
 if(isset($_GET['remove']) && Auth::authenticated()){
+  if(isset($_SESSIION['game_id']) && $_SESSIION['game_id'] === (int)$_GET['remove']){
+    unset($_SESSIION['game_id']);    
+  }
   Database::removeGame((int)$_GET['remove']);
 }
 
